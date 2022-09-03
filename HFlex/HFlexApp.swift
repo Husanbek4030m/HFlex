23132fc896a6650b1459e06b02e8a3e7def4a8f2
husanbek <husanbek4030m@gmail.com>
2022-09-03 12:02:03 +0500

    Initial Commit

---
+++ HFlex/HFlexApp.swift
@@ -0,0 +1,20 @@
+//
+//  HFlexApp.swift
+//  HFlex
+//
+//  Created by Karavella on 03/09/22.
+//
+
+import SwiftUI
+
+@main
+struct HFlexApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
